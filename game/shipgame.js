eda6ef153b51c23beb083ef8ba3d09be193fc829
HawkanZheng <hawkan.zheng@gmail.com>
2020-04-02 00:13:46 -0700

    Changed setup page and simplified arrays
    -made setup html elements has class setup
    -removed unused arrays

--- game/shipgame.js
+++ game/shipgame.js
@@ -17,12 +17,6 @@ let placeText3A = document.getElementById("setup4");
 let placeText3B = document.getElementById("setup5");
 let placeBtn = document.getElementById("place");
 
-//Array of string values for the columns
-let colArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
-
-//Battle ship coordinates
-let coorArr = [];
-
 //2D Array for user coordinates
 var userArr = new Array(T_HEIGHT);
 
@@ -136,17 +130,12 @@ function setBoard() {
 
     //Create 5 unit ship
     ship5 = createShip(coor5, SIZE5);
-    console.log(ship5.isCorrectSize());
     //Create 4 unit ships
     ship4A = createShip(coor4A, SIZE4);
-    console.log(ship4A.isCorrectSize());
     ship4B = createShip(coor4B, SIZE4);
-    console.log(ship4B.isCorrectSize());
     //Create 3 unit ships
     ship3A = createShip(coor3A, SIZE3);
-    console.log(ship3A.isCorrectSize());
     ship3B = createShip(coor3B, SIZE3); 
-    console.log(ship3B.isCorrectSize());
 
     //Check for valid ship sizes, if valid start game
     if(ship5.isCorrectSize() && ship4A.isCorrectSize() && ship4B.isCorrectSize() 
@@ -170,12 +159,10 @@ function getXCoor(str, index) {
     //Start point x cooridinate
     let startX = str[index];
     //Invalid input of first char
-    if (colArr.indexOf(startX) == -1) {
-        window.alert("Invalid input. Please re-enter coordinates.");
-        return;
-    } else {
-        coorArr.push(startX.charCodeAt(0) - OFFSET);
-    }
+    // if (colArr.indexOf(startX) == -1) {
+    //     //window.alert("Invalid input. Please re-enter coordinates.");
+    //     //return;
+    // }
     return (startX.charCodeAt(0) - OFFSET);
 }
 
@@ -183,12 +170,10 @@ function getXCoor(str, index) {
 function getYCoor(str, index) {
     //Start point y cooridinate
     let startY = parseInt(str[index]);
-    if (startY < 1 || startY > 9) {
-        window.alert("Invalid input. Please re-enter coordinates.");
-        return;
-    } else {
-        coorArr.push(startY);
-    }
+    // if (startY < 1 || startY > 9) {
+    //     window.alert("Invalid input. Please re-enter coordinates.");
+    //     return;
+    // } 
     return startY;
 }
 
@@ -199,32 +184,32 @@ function createPostitionArr() {
         userArr[i] = new Array(T_WIDTH);
         for (let j = 1; j < T_WIDTH; j++) {
 
-            //Position Ship 1
-            if (((i >= coorArr[1] && i <= coorArr[3]) || (i <= coorArr[1] && i >= coorArr[3])) &&
-                ((j >= coorArr[0] && j <= coorArr[2]) || (j <= coorArr[0] && j >= coorArr[2]))) {
+            //Position 5 unit ship 
+            if (((i >= ship5.location[1] && i <= ship5.location[3]) || (i <= ship5.location[1] && i >= ship5.location[3])) &&
+                ((j >= ship5.location[0] && j <= ship5.location[2]) || (j <= ship5.location[0] && j >= ship5.location[2]))) {
                 userArr[i][j] = 1;
             }
 
-            //Position Ship 2
-            else if (((i >= coorArr[5] && i <= coorArr[7]) || (i <= coorArr[5] && i >= coorArr[7])) &&
-                ((j >= coorArr[4] && j <= coorArr[6]) || (j <= coorArr[4] && j >= coorArr[6]))) {
+            //Position first 4 unit ship 
+            else if (((i >= ship4A.location[1] && i <= ship4A.location[3]) || (i <= ship4A.location[1] && i >= ship4A.location[3])) &&
+                ((j >= ship4A.location[0] && j <= ship4A.location[2]) || (j <= ship4A.location[0] && j >= ship4A.location[2]))) {
                 userArr[i][j] = 1;
             }
 
-            //Position Ship 3
-            else if (((i >= coorArr[9] && i <= coorArr[11]) || (i <= coorArr[9] && i >= coorArr[11])) &&
-                ((j >= coorArr[8] && j <= coorArr[10]) || (j <= coorArr[8] && j >= coorArr[10]))) {
+            //Position second 4 unit ship
+            else if (((i >= ship4B.location[1] && i <= ship4B.location[3]) || (i <= ship4B.location[1] && i >= ship4B.location[3])) &&
+                ((j >= ship4B.location[0] && j <= ship4B.location[2]) || (j <= ship4B.location[0] && j >= ship4B.location[2]))) {
                 userArr[i][j] = 1;
             }
 
-            //Position Ship 4
-            else if (((i >= coorArr[13] && i <= coorArr[15]) || (i <= coorArr[13] && i >= coorArr[15])) &&
-                ((j >= coorArr[12] && j <= coorArr[14]) || (j <= coorArr[12] && j >= coorArr[14]))) {
+            //Position first 3 unit ship
+            else if (((i >= ship3A.location[1] && i <= ship3A.location[3]) || (i <= ship3A.location[1] && i >= ship3A.location[3])) &&
+                ((j >= ship3A.location[0] && j <= ship3A.location[2]) || (j <= ship3A.location[0] && j >= ship3A.location[2]))) {
                 userArr[i][j] = 1;
             }
-            //Position Ship 5
-            else if (((i >= coorArr[17] && i <= coorArr[19]) || (i <= coorArr[17] && i >= coorArr[19])) &&
-                ((j >= coorArr[16] && j <= coorArr[18]) || (j <= coorArr[16] && j >= coorArr[18]))) {
+            //Position second 3 unit ship
+            else if (((i >= ship3B.location[1] && i <= ship3B.location[3]) || (i <= ship3B.location[1] && i >= ship3B.location[3])) &&
+                ((j >= ship3B.location[0] && j <= ship3B.location[2]) || (j <= ship3B.location[0] && j >= ship3B.location[2]))) {
                 userArr[i][j] = 1;
             } else {
                 userArr[i][j] = 0;
@@ -235,18 +220,12 @@ function createPostitionArr() {
 
 //Hide startup info
 function hideStartup() {
-    placeBtn.style.display = "none";
-    placeText5.style.display = "none";
-    placeText4A.style.display = "none";
-    placeText4B.style.display = "none";
-    placeText3A.style.display = "none";
-    placeText3B.style.display = "none";
-
-    //hide initial grid
-    document.getElementById("grid").style.display = "none";
-
-    //hide initial instructions
-    document.getElementById("enterInstruct").style.display = "none";
+    let toHide = document.getElementsByClassName("setup");
+    
+    //Hide all setup stuff
+    for (i = 0; i < toHide.length; i++){
+        toHide[i].style.display = "none";
+    }
 }
 
 placeBtn.onclick = setBoard;
